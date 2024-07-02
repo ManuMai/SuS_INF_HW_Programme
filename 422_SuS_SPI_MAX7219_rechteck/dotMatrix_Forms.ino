31121789aaea94c5287d18cfa654659583b8829a
ManuMai <github@manuel-maier.eu>
2024-07-02 08:31:20 +0200

    Update dotMatrix_Forms.ino

--- 422_SuS_SPI_MAX7219_rechteck/dotMatrix_Forms.ino
+++ 422_SuS_SPI_MAX7219_rechteck/dotMatrix_Forms.ino
@@ -1,11 +1,7 @@
 void show_rechteck_aussen()
 {
-  send_data(0x01, 0xFF); //erste Zeile alle LEDs an
-  send_data(0x02, 0x81);
-  send_data(0x03, 0x81);
-  send_data(0x04, 0x81);
-  send_data(0x05, 0x81);
-  send_data(0x06, 0x81);
-  send_data(0x07, 0x81);
-  send_data(0x08, 0xFF); // letzte Zeile...alle LEDs an
+  //Hier Code für Rechteck außen ergänzen
+
+
+
 } //Ende show_rechteck_aussen()
