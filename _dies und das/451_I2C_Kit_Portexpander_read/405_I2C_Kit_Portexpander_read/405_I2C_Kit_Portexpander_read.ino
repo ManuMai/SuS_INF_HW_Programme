fa60687e197978d922140c3d6e5d9d3074f7c682
ManuMai <github@manuel-maier.eu>
2024-07-14 19:39:18 +0200

    neue Version MCP23008 read

--- _dies und das/451_I2C_Kit_Portexpander_read/405_I2C_Kit_Portexpander_read/405_I2C_Kit_Portexpander_read.ino
+++ _dies und das/451_I2C_Kit_Portexpander_read/405_I2C_Kit_Portexpander_read/405_I2C_Kit_Portexpander_read.ino
@@ -1,6 +1,8 @@
 #include <Wire.h> //Einbinden der I2C Bibliothek
 
-#define address 0b0100000 //0x40 -> Adresse des I2C Portexpander
+#define MCP23008_adress 0b0100000 //0x40 -> Adresse des I2C Portexpander
+#define IODIR_REGISTER 0x00
+#define GPIO_REGISTER 0x09
 
 char i2c_data; //Deklaration des zu verschiebenden Bitmusters und der Zählvar i
 
@@ -12,9 +14,9 @@ void setup()
   /* einmalige Einstellung der MCP20083 - Register */
   
   Wire.begin();  //Alternatives PinMapping: Wire.begin(SDA, SCL); 
-  Wire.beginTransmission(address); // beginnt mit dem Slave-Gerät zu sprechen
-  Wire.write (0x00); // Adresse IO-Direction register (1 Byte) --> wird angesprochen
-  Wire.write (0x80); // Daten für das IO-Register(1 Byte)--> 0b1000 0000 = 80h (GP7 Input)
+  Wire.beginTransmission(MCP23008_adress); // beginnt mit dem Slave-Gerät zu sprechen
+  Wire.write (IODIR_REGISTER); // Adresse IO-Direction register (1 Byte) --> wird angesprochen
+  Wire.write (0xFF); // Daten für das IO-Register(1 Byte)--> 0b1111 1111 = FFh (Input)
   Wire.endTransmission(); // hört auf, mit dem Gerät zu sprechen --> Stop Bit
 
   Serial.println("Ende Setup");
@@ -22,7 +24,9 @@ void setup()
 
 void loop()
 {
-  Serial.print(millis()); // Gibt Millisekunden seit Start aus
+  /*Serial.print(millis()); // Gibt Millisekunden seit Start aus
+  Serial.println(": ");*/
+  delay(500);
   I2C_MCP23008_read(); // Aufruf UP I2C Daten lesen von MCP28003
  
   
---
+++ _dies und das/451_I2C_Kit_Portexpander_read/405_I2C_Kit_Portexpander_read/MCP23008_read.ino
@@ -0,0 +1,20 @@
+// Es wird immer ein Frame gesendet, der eingehalten werden muss
+
+void I2C_MCP23008_read(void)
+  {
+    Wire.beginTransmission(MCP23008_adress);
+    Wire.write(GPIO_REGISTER);
+    Wire.endTransmission();
+
+    Wire.requestFrom(MCP23008_adress, 1);    // 1 Byte vom PortexpanderIC MCP23008
+    
+    // Solange Daten im Empfangspuffer vorhanden sind
+    
+    if(Wire.available())   //Wenn Daten da sind... 
+    {
+    i2c_data = Wire.read();    // Received ein Byte und speichert es in i2c_data
+    Serial.print("aktueller Wert: ");
+    Serial.println(i2c_data,HEX);  
+    }
+
+  }
