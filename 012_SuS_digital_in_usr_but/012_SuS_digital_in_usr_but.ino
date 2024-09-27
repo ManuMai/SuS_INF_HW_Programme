c8cd698e787c7098c26a14509bc8f18eb316c408
ManuMai <github@manuel-maier.eu>
2024-09-27 08:24:37 +0200

    Create 012_SuS_digital_in_usr_but.ino

---
+++ 012_SuS_digital_in_usr_but/012_SuS_digital_in_usr_but.ino
@@ -0,0 +1,16 @@
+const int buttonPin = 2;  // Pin für die Taste S2
+int buttonState = 0;      // Variable für den Zustand der Taste
+
+void setup() {
+  Serial.begin(9600);     // Serielle Kommunikation starten
+  pinMode(buttonPin, INPUT);  // Pin D2 als Eingang festlegen
+}
+
+void loop() {
+  
+
+  // >>>>>>>> CODE ergänzen <<<<<<<<<
+
+
+  delay(100);  // Kurze Verzögerung, um das Polling zu verlangsamen
+}
