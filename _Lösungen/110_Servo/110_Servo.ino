ea21b088b49169eb3e59e32db8d3c5ee16117e8c
ManuMai <github@manuel-maier.eu>
2024-10-14 08:04:35 +0200

    Create 110_Servo.ino

---
+++ _Lösungen/110_Servo/110_Servo.ino
@@ -0,0 +1,14 @@
+#include <Servo.h>
+
+Servo myservo;  // Erstellen eines Servo-Objekts
+
+void setup() {
+  myservo.attach(D10);  // Servo an Pin PA8 anschließen
+}
+
+void loop() {
+  myservo.write(90);  // Servo auf 90 Grad positionieren
+  delay(1000);        // 1 Sekunde warten
+  myservo.write(0);   // Servo auf 0 Grad positionieren
+  delay(1000);        // 1 Sekunde warten
+}
