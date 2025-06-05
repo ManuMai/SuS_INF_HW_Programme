dfed24d16d33c6ce7d00993cf465812f0041a40f
ManuMai <github@manuel-maier.eu>
2025-06-05 10:56:08 +0200

    Update 451_I2C_Kit_Portexpander_write.ino

--- _Lösungen/451_I2C_Kit_Portexpander_write/451_I2C_Kit_Portexpander_write.ino
+++ _Lösungen/451_I2C_Kit_Portexpander_write/451_I2C_Kit_Portexpander_write.ino
@@ -1,40 +1,34 @@
-#include <Wire.h> //Einbinden der I2C Bibliothek
+#include <Wire.h> // Einbinden der I2C-Bibliothek
 
-#define address 0b0100000 //0x40 - Port Expander
+#define ADDRESS 0b0100000 // 0x40 - Port Expander
 
-char Bitmuster,i; //Deklaration des zu verschiebenden Bitmusters und der Zählvar i
-Bitmuster = 0b0000001; // Bitmuster für die LEDs
+uint8_t Bitmuster = 0b00000001; // Bitmuster für die LEDs
 
 void setup()
 {
- 
   Serial.begin(115200);  
-  /* einmalige Einstellung (Setup) der MCP23008 - Register */
   Serial.println("Ende Setup");
-  Wire.begin();  //Startet Wire Bibliothek --> Alternatives PinMapping: Wire.begin(SDA, SCL); 
-  Wire.beginTransmission(address); // beginnt mit dem Slave-Gerät zu sprechen (Write Modus)
-  Wire.write (0x00); // Auswahl des Adress IO-Direction register (1 Byte) --> Registeradresse
-  Wire.write (0x00); // Value Dies setzt alle GPIO als Ausgänge (1 Byte) -->  Daten ins REgister 0x00
-  Wire.endTransmission(); // hört auf, mit dem Gerät zu sprechen --> Stop Bit
 
-  
+  Wire.begin();  // Startet Wire-Bibliothek --> Alternatives Pin-Mapping: Wire.begin(SDA, SCL);
+  Wire.beginTransmission(ADDRESS); // beginnt mit dem Slave-Gerät zu sprechen (Write-Modus)
+  Wire.write(0x00); // Auswahl des Adress-IO-Direction-Register (1 Byte)
+  Wire.write(0x00); // Setzt alle GPIO als Ausgänge (1 Byte)
+  Wire.endTransmission(); // Stoppt die Kommunikation mit dem Gerät
 }
 
 void loop()
 {
-  
-  for(i=0;i<8;i++)
+  for(uint8_t i = 0; i < 8; i++)
   {
-  I2C_MCP23008_test();
+    I2C_MCP23008_test(); // Stelle sicher, dass diese Funktion definiert ist!
 
-  if(Bitmuster < 0xFF )
-  {
-     Bitmuster = Bitmuster << 1; //Schiebeoperator - eins nach links
+    Bitmuster <<= 1; // Schiebeoperator - eins nach links
+
+    if (Bitmuster == 0) 
+    {
+      Bitmuster = 0b00000001; // Setzt Bitmuster zurück
+    }
+
+    delay(1000);
   }
-  else {
-  Bitmuster
-  }
-   delay(1000);
-  }
-  
 }
